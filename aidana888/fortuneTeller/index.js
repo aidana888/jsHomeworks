1ca54786a0adaeb037dd3dffc09f9e358762c6e3
jyzka <jyzka1993@gmail.com>
2023-05-18 20:56:54 -0500

    homework

---
+++ aidana888/fortuneTeller/index.js
@@ -0,0 +1,5 @@
+const numberOfChildren = 3;
+const partnerName = "Zhyldyzbek";
+const geographicLocation = "Chicago";
+const jobTitle = "software engineer";
+console.log ("You will be a "+ jobTitle +" in " +geographicLocation + " and married to " + partnerName + " with " + numberOfChildren +" kids. ")
