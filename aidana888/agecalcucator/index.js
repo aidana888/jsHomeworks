dc5f1a857c6065a4ddca7c9d4c8ff538b3773785
aidana888 <aidanaj888777@gmail.com>
2023-05-18 21:34:18 -0500

    age

---
+++ aidana888/agecalcucator/index.js
@@ -0,0 +1,6 @@
+const currentYear = 2023;
+const birthYear = 1993;
+const actualAge = currentYear-birthYear;
+const possibleAge = actualAge-1;
+
+console.log("They are either "+ actualAge+ " or "+ possibleAge)
